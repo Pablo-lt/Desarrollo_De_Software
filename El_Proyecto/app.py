68677b44e24fffc5763db8a08aab604e1436e3f5
Pablo La Torre <126829651+Pablo-lt@users.noreply.github.com>
2024-08-03 20:31:43 -0300

    Add files via upload
    Modificación archivo app.py

--- El_Proyecto/app.py
+++ El_Proyecto/app.py
@@ -11,4 +11,4 @@ if __name__ == '__main__':
     Ref: https://flask.palletsprojects.com/en/3.0.x/api/#flask.Flask.run
     Ref: Book Flask Web Development Page 9
     """
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5433)
